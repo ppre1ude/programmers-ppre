030d6402e733dbdbeab2827e8618d1b26e5d5716
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-07-24 05:33:19 +0900

    [Silver I] Title: 카잉 달력, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+  int m, n, x, y;
+  cin >> m >> n >> x >> y;
+
+  int ans = -1;
+  for (int i=x; i<=m*n; i+=m) {
+    if ((i-y) % n == 0) {
+      ans = i;
+      break;
+    }
+  }
+  cout << ans << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--)
+      solve();
+  return 0;
+}
