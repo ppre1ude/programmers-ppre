0c25f7463b2052dcb3f87c494641b784687833f1
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-08-01 05:28:17 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 72 ms, Memory: 6528 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m, a, b;
+vector<vector<int>> adj;
+vector<bool> visited;
+
+void dfs(int cur) {
+  visited[cur] = true;
+  for (int i=0; i<adj[cur].size(); i++) {
+    if (visited[adj[cur][i]]) continue;
+    dfs(adj[cur][i]);
+  }
+}
+
+void solve() {
+  cin >> n >> m;
+  adj.resize(n+1, vector<int>());
+  visited.resize(n+1, false);
+    
+  for (int i=0; i<m; i++) {
+    cin >> a >> b;
+    adj[a].push_back(b);
+    adj[b].push_back(a);
+  }
+
+  int ans = 0;
+  
+  for (int i=1; i<=n; i++) {
+    if (visited[i]) continue;
+    dfs(i);
+    ans++;
+  }
+  cout << ans;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
