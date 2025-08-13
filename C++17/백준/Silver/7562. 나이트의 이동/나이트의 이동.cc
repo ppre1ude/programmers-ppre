f51e5733f712ad9821b0d444e842d0493b8db8b1
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-08-14 03:30:40 +0900

    [Silver I] Title: 나이트의 이동, Time: 20 ms, Memory: 2420 KB -BaekjoonHub

---
+++ C++17/백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <vector>
+using namespace std;
+
+int dx[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int dy[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+
+void solve() {
+  int l;
+  cin >> l;
+  pair<int, int> a, b;
+
+  cin >> a.first >> a.second;
+  cin >> b.first >> b.second;
+
+  vector<vector<int>> ans(l, vector<int>(l, -1));
+  ans[a.first][a.second] = 0;
+  queue<pair<int, int>> q;
+  q.push(a);
+ 
+  while (!q.empty()) {
+    auto [x, y] = q.front();
+    q.pop();
+
+    if (x == b.first && y == b.second) break;
+    for (int i=0; i<8; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      if (nx < 0 || nx >= l || ny < 0 || ny >= l || ans[nx][ny] != -1) continue;
+      ans[nx][ny] = ans[x][y] + 1;
+      q.push({nx, ny});
+    }
+  }
+  cout << ans[b.first][b.second] << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
