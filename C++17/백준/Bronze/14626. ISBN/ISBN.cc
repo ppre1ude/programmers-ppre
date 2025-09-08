8da914c65a1bbc5c007ec3e3c184b7b1abc4efa2
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-09-09 06:33:05 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14626. ISBN/ISBN.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void solve() {
+  string s;
+  cin >> s;
+
+  int idx = 0, sum = 0;
+  for (int i=0; i<s.length(); i++) {
+    if (s[i] == '*') idx = i;
+    else sum += (s[i] - '0') * (i & 1 ? 3 : 1);
+  }
+
+  for (int i=0; i<=9; i++) {
+    if ((sum + i * (idx & 1 ? 3 : 1)) % 10) continue;
+    cout << i;
+    return;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
