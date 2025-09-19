78e946a570fee08816451573040a434f321908c8
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-09-19 19:44:22 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 68 ms, Memory: 9948 KB -BaekjoonHub

---
+++ C++17/백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+
+  map<string, bool> a;
+  string s, t;
+  for (int i=0; i<n; i++) {
+    cin >> s >> t;
+    if (t == "enter") a[s] = true;
+    else a[s] = false;
+  }
+
+  for (auto it=a.rbegin(); it!=a.rend(); it++) {
+    if (it->second) cout << it->first << "\n";
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
