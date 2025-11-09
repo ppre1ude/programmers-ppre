290a6f5b45ac534899a9ea8f1361108440ab7be4
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-11-09 22:53:28 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 84 ms, Memory: 64808 KB -BaekjoonHub

---
+++ C++17/백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve() {
+  string a, b;
+  cin >> a;
+  cin >> b;
+
+  int ans = 0;
+  vector<vector<int> > dp(a.length(), vector<int>(b.length(), 0));
+  for (int i=0; i<a.length(); i++) {
+    for (int j=0; j<b.length(); j++) {
+      if (a[i] != b[j]) continue;
+      if (i == 0 || j == 0) dp[i][j] = 1;
+      else dp[i][j] = dp[i-1][j-1] + 1;
+      ans = max(ans, dp[i][j]);
+    }
+  }
+  cout << ans;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
