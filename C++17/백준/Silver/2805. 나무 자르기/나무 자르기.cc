e6bbd48323db657d3ce2ce2d312e4982ac16a6b5
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-11-25 15:30:47 +0900

    [Silver II] Title: 나무 자르기, Time: 152 ms, Memory: 5928 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void solve() {
+  long long n, m;
+  cin >> n >> m;
+  vector<int> a(n);
+    
+  for (int i=0; i<n; i++) cin >> a[i];
+
+  long long l = 0, r = *max_element(a.begin(), a.end());
+    
+  while (l+1 < r) {
+    long long mid = (l+r)/2, sum = 0;
+    for (int i=0; i<n; i++) {
+      if (a[i] > mid) sum += a[i] - mid;
+    }
+    if (sum >= m) l = mid;
+    else r = mid;
+  }
+  cout << l;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
