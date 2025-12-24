f0a7278ca7c6887aeae6d168e0deb3bbb8a8730b
ppre1ude <95406268+ppre1ude@users.noreply.github.com>
2025-12-24 21:40:38 +0900

    [Silver II] Title: 에디터, Time: 60 ms, Memory: 21020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+void solve() {
+  string s;
+  cin >> s;
+  list<char> l;
+    
+  for (auto c: s) {
+    l.push_back(c);
+  }
+  auto cur = l.end();
+
+  int m;
+  char command, $;
+  cin >> m;
+    
+  while (m--) {
+    cin >> command;
+    if (command == 'L') {
+      if (cur != l.begin()) cur--;
+    } else if (command == 'D') {
+      if (cur != l.end()) cur++;
+    } else if (command == 'B') {
+      if (cur != l.begin()) {
+        cur--;
+        cur = l.erase(cur);
+      }
+    } else if (command == 'P') {
+      cin >> $;
+      l.insert(cur, $);
+    }
+  }
+
+  for (auto c: l) {
+    cout << c;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+  return 0;
+}
